3f8fe6a03d76717e5c6d46bd2ee1c27e7db184da
0liver.kot <oliverkot19@gmail.com>
2025-10-21 08:54:12 +0100

    focusing with arrow keys works

--- client/app.py
+++ client/app.py
@@ -2,7 +2,6 @@ from textual import events
 from textual.app import App, ComposeResult
 from textual.widgets import Button, Header
 from textual.containers import Container, Vertical
-from enum import Enum 
 
 class MyApp(App[str]):
     TITLE = "Terminal Messaging"
@@ -23,38 +22,14 @@ class MyApp(App[str]):
         )
 
     def on_key(self, event: events.Key) -> None:
-
-        # on the first press of either up or down arrow message is focused
-        # user can then use arrows to select other options
-        if(not self.focus_intialised and (event.key == "up" or event.key == "down")):
-            self.focus_intialised = True
-            message_button = self.query_one("#message-button")
-            message_button = message_button.focus
-            return
         
-        # changing focus to different buttons with arrows
-        if(event.key == "up" or event.key == "down"):
-
-            buttons = {
-                1: self.query_one("#message-button"),
-                2: self.query_one("#friends-button"),
-                3: self.query_one("#exit-button")
-            }
+        match event.key:
             
+            case "up": self.action_focus_previous()
+            
+            case "down": self.action_focus_next()
 
-            focused = self.focused;
-            current_index = [key for key, value in buttons.items() if value == focused][0]
-
-            match event.key:
-                case "up": current_index -= 1
-                case "dowen": current_index += 1
-
-            # check if current index is valid 
-            if(current_index < 1 or current_index > 3):
-                return;
-
-            buttons[current_index] = buttons[current_index].focus
-            return
+        return 
 
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
