b82c4e26694fc69e81245bed5276adb1151dc8e2
0liver.kot <oliverkot19@gmail.com>
2025-10-21 12:04:32 +0100

    requests and friends lists can now be toggles multiple times

--- client/friends_screen.py
+++ client/friends_screen.py
@@ -7,8 +7,14 @@ class FriendsScreen(Screen):
     TITLE="Friends"
     CSS_PATH = "css/friends.tcss"  
 
+    # used in buttons to represent toggling
     requests_arrow = "^"
     friends_arrow = "^"
+
+    # flags for vertical scrolling containers 
+    # once hidden flag turns false and widget is destroyed
+    requests_vcontainer_flag = False
+    friends_vcontainer_flag = False
 
     requests = ["user 1", "user 2", "user 3", "user 4"]
     friends = ["user 1", "user 2", "user 3", "user 4"]
@@ -32,14 +38,17 @@ class FriendsScreen(Screen):
 
             return arrow
 
+
         async def mount_scroll_widget(type):
             
             if(type == "requests"):
                 list = self.requests
                 id = "requests-list"
+                self.requests_vcontainer_flag = True
             else:
                 list = self.friends
                 id = "friends-list"
+                self.friends_vcontainer_flag = True
 
             v_scroll = VerticalScroll(id=id)
             container = self.query_one("#friends-button-container", Container)
@@ -52,22 +61,41 @@ class FriendsScreen(Screen):
             
             v_scroll.refresh()
 
+
         match event.button.id:
 
             case "requests-button": 
+
+                # updating toggle arrow
                 self.requests_arrow = toggle_arrow(self.requests_arrow)
                 requests_button = self.query_one("#requests-button", Button)
                 requests_button.label = f"Requests          {self.requests_arrow}"
                 requests_button.refresh()
-                await mount_scroll_widget("requests")
+
+                # handling container showing list of requests 
+                if(self.requests_vcontainer_flag):
+                    container = self.query_one("#requests-list")
+                    await container.remove()
+                    self.requests_vcontainer_flag = False
+                else:
+                    await mount_scroll_widget("requests")
 
 
             case "friends-button": 
+
+                # updates the toggle arrow
                 self.friends_arrow = toggle_arrow(self.friends_arrow)
                 friends_button = self.query_one("#friends-button", Button)
                 friends_button.label = f"Friends          {self.friends_arrow}"
                 friends_button.refresh()      
-                await mount_scroll_widget("friends")
+                
+                # handling container showing list of friends 
+                if(self.friends_vcontainer_flag):
+                    container = self.query_one("#friends-list")
+                    await container.remove()
+                    self.friends_vcontainer_flag = False
+                else:
+                    await mount_scroll_widget("friends")
 
 
 class AddFriends(Screen):
